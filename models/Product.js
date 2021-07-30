c4c6f18624c237c8a92774a0be6e5eb601eed155
AlbertoCanales1 <albertocanales1@outlook.com>
2021-07-30 18:49:46 -0500

    attemt at fixing error

--- models/Product.js
+++ models/Product.js
@@ -22,20 +22,23 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      isDecimal: true,
+      validate: {
+        isDecimal: true,},
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      isNumeric: true
+      validate: {
+        isNumeric: true,},
+      
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
       model: 'Category',
-      key: 'id'
-      }
+      key: 'id',
+      },
     },
   },
   {
